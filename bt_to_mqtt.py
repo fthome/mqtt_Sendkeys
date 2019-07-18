7ea927b175d86607e15f5b451810ce739a9c5a46
fthome <fthome@pierron.fr>
2019-07-18 08:00:20 +0200

    Initial

---
+++ bt_to_mqtt.py
@@ -0,0 +1,28 @@
+import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+
+
+class BtToMqtt:
+    '''Une application qui attend un ordre sur un GPIO
+    et envoie un message mqtt
+    '''
+    def __init__(self, gpio_pin, topic, payload, mqtt_host = "localhost", mqtt_port = 1883):
+        self.mqtt_client = mqtt.Client("gpio_sender")
+        self.mqtt_client.connect(mqtt_host, mqtt_port)#, keepalive = 60)
+        self.gpio_pin ) gpio_pin
+        self.payload = payload
+        self.topic = topic
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(gpio_pin,GPIO.IN)
+        GPIO.add_event_detect(gpio_pin, GPIO.FALLING, callback=self.on_action, bouncetime= 500)
+
+    def on_action(self):
+        self.mqtt_client.publish(self.topic, payload = self.payload)
+
+    def run(self):
+        while true:
+            time.sleep(0.1)
+
+if __name__ == "__main__":
+    app = BtToMqtt(22, "A700/SendKeys", "^'")
+    app.run()
---
+++ mqtt_sendkeys.py
@@ -0,0 +1,34 @@
+import win32com.client
+import time
+import paho.mqtt.client as mqtt
+
+class MqttSendKeys:
+    '''Une application qui attend des ordres issu de messages mqtt
+    pour les envoyer vers une application windows
+    '''
+    def __init__(self, application_name, mqtt_topic, mqtt_host = "localhost", mqtt_port = 1883):
+        self.mqtt_client = mqtt.Client("Sendkeys")
+        self.mqtt_client.on_message = self.on_message
+        self.mqtt_client.connect(mqtt_host, mqtt_port)#, keepalive = 60)
+        self.mqtt_client.subscribe(mqtt_topic, qos = 0)
+        self.application_name = application_name
+        self.shell = win32com.client.Dispatch("WScript.Shell")
+
+    def run(self):
+        self.mqtt_client.loop_forever()
+
+
+    def on_message(self, client, userdata, message):
+        print("message received " ,str(message.payload.decode("utf-8")))
+        print("message topic=",message.topic)
+        if self.shell.AppActivate(self.application_name):
+            print("Send : %s send to %s"%(message.payload, self.application_name))
+            self.shell.SendKeys(message.payload.decode("utf-8"),1)
+        else:
+            print("Oups, application not active!")
+
+
+if __name__ == "__main__":
+    print('Start Send keys....')
+    app = MqttSendKeys("RasterLinkPro5IP", "A700/SendKeys", mqtt_host = "192.9.200.170")
+    app.run()
