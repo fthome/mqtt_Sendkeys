1767e04e6934eb3d05a066859ce8ab9409ac4a93
fthome <fthome@pierron.fr>
2019-08-28 11:42:28 +0200

    Autoreconnect

--- mqtt_sendkeys.py
+++ mqtt_sendkeys.py
@@ -7,20 +7,29 @@ class MqttSendKeys:
     pour les envoyer vers une application windows
     '''
     def __init__(self, application_name, mqtt_topic, mqtt_host = "localhost", mqtt_port = 1883):
+        self.mqtt_topic = mqtt_topic
         self.mqtt_client = mqtt.Client("Sendkeys")
         self.mqtt_client.on_message = self.on_message
         self.mqtt_client.on_disconnect = self.on_disconnect
+        self.mqtt_client.on_connect = self.on_connect
+        self.mqtt_client.on_subscribe = self.on_subscribe
         self.mqtt_client.connect(mqtt_host, mqtt_port)#, keepalive = 60)
-        self.mqtt_client.subscribe(mqtt_topic, qos = 0)
         self.application_name = application_name
         self.shell = win32com.client.Dispatch("WScript.Shell")
 
     def run(self):
         self.mqtt_client.loop_forever()
 
+    def on_connect(self,mqttc, obj, flags, rc):
+        print("MQTT connection! Subscribe to %s. (rc : %s)"%(self.mqtt_topic,str(rc)))
+        self.mqtt_client.subscribe(self.mqtt_topic, qos = 0)
+
     def on_disconnect(self, client, userdata, rc):
         if rc != 0:
             print("Unexpected MQTT disconnection. Will auto-reconnect")
+
+    def on_subscribe(self, mqttc, obj, mid, granted_qos):
+        print("Subscribed: "+str(mid)+" "+str(granted_qos))
 
     def on_message(self, client, userdata, message):
         print("message received " ,str(message.payload.decode("utf-8")))
